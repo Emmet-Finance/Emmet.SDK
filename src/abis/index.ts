0b90b3b5ac480a08ea50ab7214fc9597ce639798
Mithras <dimabrook79@gmail.com>
2023-07-07 17:20:52 +0300

    misc

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,7 +1,7 @@
-import BUSD     from "./BUSD";
-import DAI      from "./DAI";
+import BUSD     from "./busd";
+import DAI      from "./dai";
 import FTBridge from "./FTBridge";
-import USDT     from "./USDT";
+import USDT     from "./usdt";
 import WERC20   from "./WrappedERC20";
 
 export {
