6ede83654185232f69430a06ec930a553fb3dc40
Mithras <dimabrook79@gmail.com>
2023-07-08 16:05:50 +0300

    add wallet interface

---
+++ src/interfaces/wallet.ts
@@ -0,0 +1,15 @@
+import { ethers } from 'ethers';
+
+export interface IConnectedWallet {
+    isConnected: boolean;
+    address?: string;
+    balance?: {
+        value: string;
+        formatted: string;
+    };
+}
+
+export interface IWalletProvider {
+    getWeb3Provider: () => Promise<ethers.providers.Web3Provider>;
+    isMobile: () => boolean;
+}
