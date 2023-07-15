526de6e6f50328669d59a746053831c014dc30a8
Mithreum <120666231+Mithreum@users.noreply.github.com>
2023-07-15 13:19:07 +0000

    Build: (7dac75c) fix: *.js not included

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,4 @@ import { BUSD, DAI, FTBridge, USDT, WERC20 } from './abis/index.js';
 import { CHAIN_LOGOS, TOKEN_LOGOS, } from './logos/index.js';
 import { EVMMainnets, EVMTestnets, EVMMainnetNames, EVMTestnetNames, MAINNET_CHAIN_IDS, MAINNET_CHAIN_ID_TO_NAME, TESTNET_CHAIN_IDS, TESTNET_CHAIN_ID_TO_NAME, } from './types/index.js';
 import { TEnvironment } from './enums/environment.js';
-import { log } from 'console';
 export { BUSD, DAI, FTBridge, USDT, WERC20, CHAIN_LOGOS, TOKEN_LOGOS, TEnvironment, EVMMainnets, EVMTestnets, EVMMainnetNames, EVMTestnetNames, MAINNET_CHAIN_IDS, MAINNET_CHAIN_ID_TO_NAME, TESTNET_CHAIN_IDS, TESTNET_CHAIN_ID_TO_NAME, };
-log(MAINNET_CHAIN_IDS);
-log(MAINNET_CHAIN_ID_TO_NAME);
