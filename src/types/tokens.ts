9d101bba0e7c6cdd059cd7a51e30eca9f1814fe8
Mithras <dimabrook79@gmail.com>
2023-07-18 08:02:09 +0300

    add: token name typings

--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -1,8 +1,16 @@
+import { mainnetTokens, testnetTokens } from '../tokens';
+
+/**
+ * Contract address type
+ */
 export type ContractAddress = {
     address:string,
     chain:string
 }
 
+/**
+ * Type defining a supported token structure
+ */
 export type SupportedTokenType = {
     abi:string, // Native contract ABI
     decimals: number,
@@ -11,3 +19,23 @@ export type SupportedTokenType = {
     native: ContractAddress,
     wrapped: ContractAddress[]
 }
+
+/**
+ * Type restricting token names
+ */
+export type TMainnetTokenNames = keyof typeof mainnetTokens;
+
+/**
+ * Array of Mainnet token names
+ */
+export const MainnetTokenNames: TMainnetTokenNames[] = Object.keys(mainnetTokens) as TMainnetTokenNames[];
+
+/**
+ * Type restricting token names
+ */
+export type TTestnetTokenNames = keyof typeof testnetTokens;
+
+/**
+ * Array of testnet token names
+ */
+export const TestnetTokenNames: TTestnetTokenNames[] = Object.keys(testnetTokens) as TTestnetTokenNames[]
