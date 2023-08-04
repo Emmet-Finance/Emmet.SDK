53070e7f66a442b82104f2c25e5712691ab4327c
Mithras <dimabrook79@gmail.com>
2023-08-04 12:00:52 +0300

    update: src/interfaces/wallet.ts

--- src/interfaces/wallet.ts
+++ src/interfaces/wallet.ts
@@ -7,6 +7,27 @@ export interface IConnectedWallet {
     };
 }
 
-export interface Window {
-    ethereum: any
+export interface RequestArguments {
+    method: string;
+    params?: any[];
 }
+
+export interface EthereumProvider {
+    isMetaMask?: boolean;
+    isCoinBase?: boolean;
+    once(eventName: string | symbol, listener: (...args: any[]) => void): this;
+    on(eventName: string | symbol, listener: (...args: any[]) => void): this;
+    off(eventName: string | symbol, listener: (...args: any[]) => void): this;
+    addListener(eventName: string | symbol, listener: (...args: any[]) => void): this;
+    removeListener(eventName: string | symbol, listener: (...args: any[]) => void): this;
+    removeAllListeners(event?: string | symbol): this;
+    request<T = unknown>(args: RequestArguments): Promise<T>
+}
+
+declare global {
+    export interface Window {
+        ethereum?: EthereumProvider;
+    }
+}
+
+export declare let window: Window & typeof globalThis;
