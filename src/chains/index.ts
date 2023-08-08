3889f238ba0c784b105d42cab6c7cdea49e41522
Mithras <dimabrook79@gmail.com>
2023-08-09 01:08:02 +0300

    join access to testnet & mainnet chains

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -1,10 +1,41 @@
 // Mainnets:
-export {bsc} from './bsc';
-export {ethereum} from './ethereum';
-export {fuse} from './fuse'
-export {polygon} from './polygon';
+import {bsc} from './bsc';
+import {ethereum} from './ethereum';
+import {fuse} from './fuse'
+import {polygon} from './polygon';
 // Testnets:
-export {bscTestnet} from './bscTestnet';
-export {goerli} from './goerly';
-export {mumbai} from './mumbai';
-export {sparknet} from './sparknet';
+import {bscTestnet} from './bscTestnet';
+import {goerli} from './goerly';
+import {mumbai} from './mumbai';
+import {sparknet} from './sparknet';
+
+const MAINNETS = {
+    bsc,
+    ethereum,
+    fuse,
+    polygon
+}
+
+const TESTNETS = {
+    bsctestnet: bscTestnet,
+    goerli,
+    mumbai,
+    sparknet
+}
+
+const ALL_CHAINS = MAINNETS && TESTNETS;
+
+export {
+    ALL_CHAINS,
+    MAINNETS,
+    bsc,
+    ethereum,
+    fuse,
+    polygon,
+
+    TESTNETS,
+    bscTestnet,
+    goerli,
+    mumbai,
+    sparknet,
+}
