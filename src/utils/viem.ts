451160eb088e17358a3a674195de0fc8f5f562c9
Mithras <dimabrook79@gmail.com>
2023-08-10 23:55:15 +0300

    add: getEvmTokenAllowances, getEvmTokenBalances

--- src/utils/viem.ts
+++ src/utils/viem.ts
@@ -6,7 +6,16 @@ import {
     PublicClient,
 } from 'viem';
 import { isEvmAddress } from './evm';
-import { EVMChain, TChainName } from '../types';
+import { 
+    EVMChain,
+    TChainName, 
+    TTestnetTokenNames, 
+    TestnetTokenNames, 
+    TokenBalanceObject, 
+    testnets 
+} from '../types';
+import { testnetTokens } from '../tokens';
+import { TESTNETS } from '../chains';
 
 
 /**
@@ -19,13 +28,94 @@ import { EVMChain, TChainName } from '../types';
 export function getContract(
     address: string,
     abi: any,
-    publicClient: PublicClient
+    publicClient: PublicClient | undefined
 ) {
     return getViemContract({
         address: `0x${address.slice(2)}`,
         abi,
         publicClient,
     });
+}
+
+
+/**
+ * Loops through Token contracts & extracts user allowances
+ * @param account checked account
+ * @returns an empty or filled `allowances` Object
+ */
+export async function getEvmTokenAllowances(
+    account: string,
+    chainName: TChainName
+): Promise<TokenBalanceObject | undefined> {
+
+    let allowances: { [key: TTestnetTokenNames | string]: string } = {};
+
+    if (isEvmAddress(account) && chainName) {
+
+        const publicClient = getPublicClient(account, chainName, testnets, true);
+
+        for await (const tokenName of TestnetTokenNames) {
+            const token = testnetTokens[tokenName]
+
+            const address: string = token.address[chainName.toLowerCase()];
+
+            if (address) {
+
+                const contract = getContract(
+                    address,
+                    token.abi,
+                    publicClient,
+                );
+
+                const testnet = TESTNETS[chainName.toLowerCase().replace(/[^a-zA-Z]/g, '') as keyof typeof TESTNETS];
+
+                allowances[tokenName] = (await contract.read.allowance([
+                    account,
+                    testnet.bridge.toString()
+                ])).toString();
+            }
+
+        }
+    }
+    return allowances || undefined;
+}
+
+
+/**
+ * Loops through Token contracts & extracts user balances
+ * @param account checked account
+ * @returns an empty or filled `balances` Object
+ */
+export async function getEvmTokenBalances(
+    account: string,
+    chainName: TChainName
+): Promise<TokenBalanceObject | undefined> {
+
+    let balances: { [key: TTestnetTokenNames | string]: string } = {};
+
+    if (isEvmAddress(account) && chainName) {
+
+        const publicClient = getPublicClient(account, chainName, testnets, true);
+
+        for await (const tokenName of TestnetTokenNames) {
+
+            const token = testnetTokens[tokenName]
+
+            const address: string = token.address[chainName.toLowerCase().replace(' ', '')];
+
+            if (address && publicClient) {
+
+                const contract = getContract(
+                    address,
+                    token.abi,
+                    publicClient,
+                );
+
+                balances[tokenName] = (await contract.read.balanceOf([account])).toString();;
+            }
+        }
+    }
+    return balances || undefined;
 }
 
 
