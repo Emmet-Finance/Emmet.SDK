8634c58871a4fd530f731ff37570c69567b06533
Mithreum <120666231+Mithreum@users.noreply.github.com>
2023-08-12 11:10:19 +0000

    Build: (f74acb0) add: estimateSend

--- dist/types/chains.d.ts
+++ dist/types/chains.d.ts
@@ -19,3 +19,21 @@ export declare let EVMMainnets: EVMChainMap;
 export declare let EVMTestnets: EVMChainMap;
 export declare const ChainIdToName: ChainIdToNameMap;
 export declare const AllChainNamesToId: ChainNameToId;
+export declare const mainnetChainNameToIndex: {
+    [key: string]: number;
+};
+export declare const mainnetChainIndexToName: {
+    [key: number]: string;
+};
+export declare const testnetChainNameToIndex: {
+    [key: string]: number;
+};
+export declare const testnetChainIndexToName: {
+    [key: number]: string;
+};
+export declare const allChainNameToIndex: {
+    [key: string]: number;
+};
+export declare const allChainIndexToName: {
+    [key: number]: string;
+};
--- dist/types/chains.js
+++ dist/types/chains.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AllChainNamesToId = exports.ChainIdToName = exports.EVMTestnets = exports.EVMMainnets = exports.testnetChainIdToName = exports.mainnetChainIdToName = exports.testnetChainIds = exports.mainnetChainIds = exports.testnets = exports.mainnets = void 0;
+exports.allChainIndexToName = exports.allChainNameToIndex = exports.testnetChainIndexToName = exports.testnetChainNameToIndex = exports.mainnetChainIndexToName = exports.mainnetChainNameToIndex = exports.AllChainNamesToId = exports.ChainIdToName = exports.EVMTestnets = exports.EVMMainnets = exports.testnetChainIdToName = exports.mainnetChainIdToName = exports.testnetChainIds = exports.mainnetChainIds = exports.testnets = exports.mainnets = void 0;
 const chains_1 = require("../chains");
 exports.mainnets = [
     chains_1.bsc,
@@ -32,4 +32,30 @@ exports.testnets.map((net) => {
 });
 exports.ChainIdToName = exports.mainnetChainIdToName && exports.testnetChainIdToName;
 exports.AllChainNamesToId = exports.mainnetChainIds && exports.testnetChainIds;
+exports.mainnetChainNameToIndex = {
+    'Ethereum': 1,
+    'BSC': 2,
+    'Polygon': 3,
+    'Fuse': 4
+};
+exports.mainnetChainIndexToName = {
+    1: 'Ethereum',
+    2: 'BSC',
+    3: 'Polygon',
+    4: 'Fuse'
+};
+exports.testnetChainNameToIndex = {
+    'Goerly': 1,
+    'BSC Testnet': 2,
+    'Mumbai': 3,
+    'Sparknet': 4
+};
+exports.testnetChainIndexToName = {
+    1: 'Goerly',
+    2: 'BSC Testnet',
+    3: 'Mumbai',
+    4: 'Sparknet'
+};
+exports.allChainNameToIndex = exports.mainnetChainNameToIndex && exports.testnetChainNameToIndex;
+exports.allChainIndexToName = exports.mainnetChainIndexToName && exports.testnetChainIndexToName;
 //# sourceMappingURL=chains.js.map
