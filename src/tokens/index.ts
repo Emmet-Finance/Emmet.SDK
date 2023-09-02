8c5d3f32808ce888cf176c95c7e87ae763ee5c24
Mithras <dimabrook79@gmail.com>
2023-09-02 20:25:27 +0300

    add: ETH to the Emmet.SDK

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -1,3 +1,6 @@
+// Coins
+import testnet_ETH from './testnet/eth';
+// Tokens
 import testnet_BUSD from './testnet/busd';
 import testnet_DAI from './testnet/dai';
 import testnet_USDT from './testnet/usdt';
@@ -6,6 +9,9 @@ import testnet_USDT from './testnet/usdt';
  * Hashmap: testnet tokens
  */
 export const testnetTokens = {
+    // Coins
+    ETH: testnet_ETH,
+    // Tokans
     BUSD: testnet_BUSD,
     DAI: testnet_DAI,
     USDT: testnet_USDT
