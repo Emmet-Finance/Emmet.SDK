50f1931a691f14cce733fc3cf9f9b12535980f81
Mithras <dimabrook79@gmail.com>
2023-09-02 20:23:35 +0300

    add: ETH native coin

---
+++ src/tokens/testnet/eth.ts
@@ -0,0 +1,22 @@
+import WERC20 from '../../abi/wrappedERC20';
+import { LOGO_ETH } from '../../logos/tokens';
+import { ContractAddress, SupportedTokenType } from '../../types';
+
+const ETH: SupportedTokenType = {
+    address: {
+        // Native
+        goerli:"",
+        // Wrapped
+        bsctestnet:"0xB7179d40FD12C84C300c1433bc93Cfcc388895fD",
+        mumbai:"0xeC3f5eE48d8F592d57cFF674885AdF82BeE02cF7",
+        sparknet:"0x239e6A9C05e55066C7e0fF899a32AA06A3AeafBD",
+        athens3:"0x9FAdC446f0968946a1242D98E640a7A84c4D4A80"
+    } as ContractAddress,
+    abi: WERC20,
+    decimals: 18,
+    logo: LOGO_ETH,
+    name: "ETH",
+    native: "goerli"
+}
+
+export default ETH;
