8d3fadcced87443fce1f4c62e593e4ee898f7bd5
Mithreum <120666231+Mithreum@users.noreply.github.com>
2023-10-03 13:53:52 +0000

    Build: (3b01753) update ERC20 tokens on Base Goerli

--- dist/tokens/testnet/busd.js
+++ dist/tokens/testnet/busd.js
@@ -12,7 +12,7 @@ const BUSD = {
         mumbai: "0xCbD10d57EE3bc0088B3db30777E8b415C95f3747",
         sparknet: "0xbF99037F929222cc7C0aa67acd91A12f4782b96A",
         athens3: "0x4D2E1a2Dd8fd4F60295d4F46F3B783dC5b5570F3",
-        basegoerli: "0x4D2E1a2Dd8fd4F60295d4F46F3B783dC5b5570F3",
+        basegoerli: "0xF59bbE6673F8716879ad60bb961f717C718dbF90",
         lineatestnet: "0xA0C581aA8656277Fa390b0a234C7738514c99949",
         shardeumbetanet: "0x4d2e1a2dd8fd4f60295d4f46f3b783dc5b5570f3",
     },
